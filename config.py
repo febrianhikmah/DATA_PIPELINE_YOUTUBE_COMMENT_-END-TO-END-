9d43d2a16b3cee7078160dcbe04b3c447ad59637
febrianhikmah <febriannurrohim12@gmail.com>
2026-04-20 23:02:22 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,19 @@
+API_KEY = "AIzaSyB1duIkhY6t032IpNTGGzQQ9vngA2ihE_g"
+
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "youtube_dw",
+    "user": "postgres",
+    "password": "Febrian02", # lUyPEvZ6rOFtrSJA
+    "port": 5432
+}
+
+# API_KEY = "AIzaSyB1duIkhY6t032IpNTGGzQQ9vngA2ihE_g"
+
+# DB_CONFIG = {
+#     "host": "db.xcqkcjvrawrorjnsgeew.supabase.co",
+#     "database": "postgres",
+#     "user": "postgres",
+#     "password": "lUyPEvZ6rOFtrSJA",
+#     "port": 5432
+# }
